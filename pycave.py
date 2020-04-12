7d8a54c5f0c61dbb7fdddfe112f533ac26f1493e
darkpills <darkpills>
2020-04-12 23:51:57 +0200

    Adding intersection code cave detection Adding ability to change the lookup byte

--- pycave.py
+++ pycave.py
@@ -10,8 +10,26 @@ import argparse
 import pefile
 import sys
 
+def getCharacteristics(characteristics):
+    result = ""
+    
+    if (characteristics & 0x40000000):
+        result+="R"
+    else:
+        result+="-"
+    if (characteristics & 0x40000000):
+        result+="W"
+    else:
+        result+="-"
+    if (characteristics & 0x00000020 > 0 or characteristics & 0x20000000 > 0):
+        result+="X"
+    else:
+        result+="-"
 
-def pycave(file_name, cave_size, base):
+    return result
+
+
+def pycave(file_name, cave_size, lookupByte, base):
 
     image_base = int(base, 16)
     min_cave = cave_size
@@ -39,8 +57,26 @@ def pycave(file_name, cave_size, base):
 
     fd = open(fname, "rb")
 
-    print("\n[+] Looking for code caves...")
+    VAEnd = 0
+
+    print("\n[+] Looking for code caves of 0x%02X..." % lookupByte)
     for section in pe.sections:
+
+
+        # Print section data
+        RAStart = section.PointerToRawData
+        RAEnd = section.PointerToRawData + section.SizeOfRawData
+        VAStart = image_base + section.VirtualAddress
+
+        # Special case where we have space between sections
+        if VAEnd != 0 and VAStart - VAEnd >= min_cave and lookupByte == 0x00:
+                print("[+]\tCode cave found \tSize: %d bytes \tRA: N/A \tVA: 0x%08X (intersection space)"
+                    % ((VAStart - VAEnd), VAEnd))
+
+        VAEnd = image_base + section.VirtualAddress + section.Misc_VirtualSize
+        print("[i] %s \tRA: 0x%08X-0x%08X\tVA: 0x%08X-0x%08X\tFlags: %s" 
+            % (section.Name.decode(), RAStart, RAEnd, VAStart, VAEnd, getCharacteristics(section.Characteristics)))
+
         if section.SizeOfRawData != 0:
             pos = 0
             count = 0
@@ -49,21 +85,25 @@ def pycave(file_name, cave_size, base):
 
             for byte in data:
                 pos += 1
-                if byte == 0x00:
+                if byte == lookupByte:
                     count += 1
                 else:
-                    if count > min_cave:
-                        raw_addr = section.PointerToRawData + pos - count - 1
-                        vir_addr = image_base + section.VirtualAddress + pos - count - 1
+                    if count >= min_cave:
+                        raw_addr = RAStart + pos - count - 1
+                        vir_addr = VAStart + pos - count - 1
 
-                        print("[+] Code cave found in %s \tSize: %d bytes \tRA: 0x%08X \tVA: 0x%08X"
-                              % (section.Name.decode(), count, raw_addr, vir_addr))
+                        print("[+]\tCode cave found \tSize: %d bytes \tRA: 0x%08X \tVA: 0x%08X"
+                              % (count, raw_addr, vir_addr))
                     count = 0
 
     pe.close()
     fd.close()
 
 if __name__ == "__main__":
+
+    if sys.version_info[0] < 3:
+        raise Exception("Use using Python 3")
+
     '''This function parses and return arguments passed in'''
     # Assign description to the help doc
     parser = argparse.ArgumentParser(
@@ -76,17 +116,15 @@ if __name__ == "__main__":
     parser.add_argument("-s", "--size", dest="size", action="store", default=300,
                         help="Min. cave size", type=int)
 
-    parser.add_argument("-b", "--base", dest="base", action="store", default="0x00400000",
+    parser.add_argument("-b", "--byte", dest="lookupByte", action="store", default="0x00",
+                        help="Lookup byte in cave, format 0x00", type=str)
+
+    parser.add_argument("-i", "--image-base", dest="imageBase", action="store", default="0x00400000",
                         help="Image base", type=str)
 
     args = parser.parse_args()
-
     if args.file_name:
-        pycave(args.file_name, args.size, args.base)
-    elif args.size:
-        pycave(args.file_name, args.size, args.base)
-    elif args.base:
-        pycave(args.file_name, args.size, args.base)
+        pycave(args.file_name, args.size, int(args.lookupByte[2:], 16), args.imageBase)
     else:
         parser.print_help()
         exit(-1)
